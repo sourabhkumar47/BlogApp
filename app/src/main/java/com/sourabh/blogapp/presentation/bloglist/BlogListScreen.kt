b20b21ed2000c8e9fa74536c1650aa45cdaacd44
Sourabh Kumar <sourabh.47512@gmail.com>
2024-10-07 03:58:46 +0530

    added top app bar

--- app/src/main/java/com/sourabh/blogapp/presentation/bloglist/BlogListScreen.kt
+++ app/src/main/java/com/sourabh/blogapp/presentation/bloglist/BlogListScreen.kt
@@ -1,32 +1,17 @@
 package com.sourabh.blogapp.presentation.bloglist
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.height
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.Card
-import androidx.compose.material3.CardDefaults
-import androidx.compose.material3.CircularProgressIndicator
-import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
 import com.sourabh.blogapp.data.remote.responseItem
@@ -43,40 +28,45 @@ fun BlogListScreen(
     val blogs by viewModel.blogs.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
 
-//    TopAppBar(
-//        title = { Text(text = "Blogs") },
-//        colors = TopAppBarDefaults.topAppBarColors(
-//            containerColor = MaterialTheme.colorScheme.onSurfaceVariant,
-//            titleContentColor = MaterialTheme.colorScheme.background
-//        )
-//    )
-
-    Box(modifier = Modifier.fillMaxSize()) {
-        LazyColumn(
-            modifier = Modifier.fillMaxSize(),
-            contentPadding = PaddingValues(16.dp),
-            verticalArrangement = Arrangement.spacedBy(8.dp)
-        ) {
-            items(blogs) { blog ->
-                BlogListItem(blog) {
-                    val encodedUrl = URLEncoder.encode(blog.link, StandardCharsets.UTF_8.toString())
-                    navController.navigate("blogDetail/$encodedUrl")
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = { Text(text = "Blog App") },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.onSurfaceVariant,
+                    titleContentColor = Color.White
+                )
+            )
+        }
+    ) { paddingValues ->
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .padding(paddingValues)) {
+            LazyColumn(
+                modifier = Modifier.fillMaxSize(),
+                contentPadding = PaddingValues(16.dp),
+                verticalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                items(blogs) { blog ->
+                    BlogListItem(blog) {
+                        val encodedUrl = URLEncoder.encode(blog.link, StandardCharsets.UTF_8.toString())
+                        navController.navigate("blogDetail/$encodedUrl")
+                    }
                 }
+            }
+
+            if (isLoading) {
+                CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
             }
         }
 
-        if (isLoading) {
-            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-        }
-    }
-
-    LaunchedEffect(blogs) {
-        if (blogs.isNotEmpty() && !isLoading) {
-            viewModel.loadMoreBlogs()
+        LaunchedEffect(blogs) {
+            if (blogs.isNotEmpty() && !isLoading) {
+                viewModel.loadMoreBlogs()
+            }
         }
     }
 }
-
 
 @Composable
 fun BlogListItem(blog: responseItem, onClick: () -> Unit) {
@@ -90,7 +80,7 @@ fun BlogListItem(blog: responseItem, onClick: () -> Unit) {
         Column(
             modifier = Modifier.padding(16.dp)
         ) {
-            Text(text = blog.title.rendered, style = MaterialTheme.typography.headlineSmall)
+            Text(text = blog.title.rendered, style = MaterialTheme.typography.titleLarge)
         }
     }
 }
