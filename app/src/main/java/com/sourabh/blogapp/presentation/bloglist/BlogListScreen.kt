b5a7fc0384f69bbbf493c934fb1070077f50e68c
Sourabh Kumar <sourabh.47512@gmail.com>
2024-10-07 03:55:49 +0530

    removed top app bar

--- app/src/main/java/com/sourabh/blogapp/presentation/bloglist/BlogListScreen.kt
+++ app/src/main/java/com/sourabh/blogapp/presentation/bloglist/BlogListScreen.kt
@@ -5,16 +5,22 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -28,6 +34,7 @@ import com.sourabh.blogapp.viewmodel.BlogViewModel
 import java.net.URLEncoder
 import java.nio.charset.StandardCharsets
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun BlogListScreen(
     navController: NavController,
@@ -36,34 +43,40 @@ fun BlogListScreen(
     val blogs by viewModel.blogs.collectAsState()
     val isLoading by viewModel.isLoading.collectAsState()
 
-    LazyColumn(
-        modifier = Modifier.fillMaxSize(),
-        contentPadding = PaddingValues(16.dp),
-        verticalArrangement = Arrangement.spacedBy(8.dp)
-    ) {
-        items(blogs) { blog ->
-            BlogListItem(blog) {
-                val encodedUrl = URLEncoder.encode(blog.link, StandardCharsets.UTF_8.toString())
-                navController.navigate("blogDetail/$encodedUrl")
+//    TopAppBar(
+//        title = { Text(text = "Blogs") },
+//        colors = TopAppBarDefaults.topAppBarColors(
+//            containerColor = MaterialTheme.colorScheme.onSurfaceVariant,
+//            titleContentColor = MaterialTheme.colorScheme.background
+//        )
+//    )
+
+    Box(modifier = Modifier.fillMaxSize()) {
+        LazyColumn(
+            modifier = Modifier.fillMaxSize(),
+            contentPadding = PaddingValues(16.dp),
+            verticalArrangement = Arrangement.spacedBy(8.dp)
+        ) {
+            items(blogs) { blog ->
+                BlogListItem(blog) {
+                    val encodedUrl = URLEncoder.encode(blog.link, StandardCharsets.UTF_8.toString())
+                    navController.navigate("blogDetail/$encodedUrl")
+                }
             }
         }
 
-        item {
-            if (isLoading) {
-                Box(modifier = Modifier.fillMaxWidth()) {
-                    CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
-                }
-            } else {
-                // Trigger load more when the user scrolls to the end
-                LaunchedEffect(blogs) {
-                    if (blogs.isNotEmpty()) {
-                        viewModel.loadMoreBlogs()
-                    }
-                }
-            }
+        if (isLoading) {
+            CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
+        }
+    }
+
+    LaunchedEffect(blogs) {
+        if (blogs.isNotEmpty() && !isLoading) {
+            viewModel.loadMoreBlogs()
         }
     }
 }
+
 
 @Composable
 fun BlogListItem(blog: responseItem, onClick: () -> Unit) {
